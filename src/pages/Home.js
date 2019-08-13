311c2262b8a4b6be98eb0d738cc28a8c5a70a921
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-13 11:28:02 -0700

    reordered projects to have most impressive first

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,32 +52,30 @@ function Home() {
         <h2>
           <span className="flare">&#9788;</span> Projects <span className="flare">&#9788;</span>
         </h2>
+
         <div className="proj">
           <h3>
-            Shappit
+            Pokemon Party Planner
           </h3>
           <p>
-            Oftentimes the way shipping organizations are set up makes the user use several different software simultaneously to get a single job complete. The idea behind my app is that shipping should be a one-stop shop, whether the user is an employee or customer of the shipping line. On the back end I set up a Django app acting as an API with a PostgreSQL database. The front end is an additional decoupled Django app that submits all data changes via said backend API, and displays returned data appropriately using Django's templating language, DTL.
+            Nintendo's Pokemon franchise has an ultra-popular series of home and mobile games that have had followers young and old for over two decades. Some are content with a casual play style, but there is also a competitive scene wherein a lot of strategy goes into the formation of a team -- a so-called "party" -- of six Pokemon.  When you haven't played through the games enough times to know what kind of Pokemon to use, or you want to make side-by-side comparisons of multiple possible party configurations, this app will drastically shorten the length of time you need to do so, while giving you all the data you need to make informed strategic decisions.
           </p>
           <div>
-            <a href="https://shappit.herokuapp.com">
+            <a href="https://pokemonpartyplanner.herokuapp.com">
               <div className="projlink projsite">
                 Live site
               </div>
             </a>
-            <a href="https://github.com/nathanalexanderpage/shappit-frontend">
+            <a href="https://github.com/nathanalexanderpage/pokemon-party-builder">
               <div className="projlink projgithub">
-                Github frontend repo
-              </div>
-            </a>
-            <a href="https://github.com/nathanalexanderpage/shappit-backend">
-              <div className="projlink projgithub">
-                Github backend repo
+                Github repo
               </div>
             </a>
           </div>
         </div>
+
         <hr />
+
         <div className="proj">
           <h3>
             Kero
@@ -108,20 +106,25 @@ function Home() {
 
         <div className="proj">
           <h3>
-            Pokemon Party Planner
+            Shappit
           </h3>
           <p>
-            Nintendo's Pokemon franchise has an ultra-popular series of home and mobile games that have had followers young and old for over two decades. Some are content with a casual play style, but there is also a competitive scene wherein a lot of strategy goes into the formation of a team -- a so-called "party" -- of six Pokemon.  When you haven't played through the games enough times to know what kind of Pokemon to use, or you want to make side-by-side comparisons of multiple possible party configurations, this app will drastically shorten the length of time you need to do so, while giving you all the data you need to make informed strategic decisions.
+            Oftentimes the way shipping organizations are set up makes the user use several different software simultaneously to get a single job complete. The idea behind my app is that shipping should be a one-stop shop, whether the user is an employee or customer of the shipping line. On the back end I set up a Django app acting as an API with a PostgreSQL database. The front end is an additional decoupled Django app that submits all data changes via said backend API, and displays returned data appropriately using Django's templating language, DTL.
           </p>
           <div>
-            <a href="https://pokemonpartyplanner.herokuapp.com">
+            <a href="https://shappit.herokuapp.com">
               <div className="projlink projsite">
                 Live site
               </div>
             </a>
-            <a href="https://github.com/nathanalexanderpage/pokemon-party-builder">
+            <a href="https://github.com/nathanalexanderpage/shappit-frontend">
               <div className="projlink projgithub">
-                Github repo
+                Github frontend repo
+              </div>
+            </a>
+            <a href="https://github.com/nathanalexanderpage/shappit-backend">
+              <div className="projlink projgithub">
+                Github backend repo
               </div>
             </a>
           </div>
