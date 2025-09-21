47e9de5275c7b0223cc04d76628be230ee7b752e
nathanalexanderpage <49886813+nathanalexanderpage@users.noreply.github.com>
2025-09-21 01:33:05 -0700

    update deps, bring syntax up to date

--- src/navigation/Content.js
+++ src/navigation/Content.js
@@ -3,17 +3,17 @@ import About from '../pages/About';
 import Blog from '../pages/Blog';
 import Projects from '../pages/Projects';
 import Home from '../pages/Home';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 class Content extends Component {
   render () {
     return (
-      <div className="content-holder">
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/projects" component={Projects} />
-      </div>
+      <Routes className="content-holder">
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     )
   }
 }
